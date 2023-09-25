72ee616ad47cd84d4d1c3e8ef9c10eef322e8932
marcelmroc <marcelosticorrea@gmail.com>
2023-09-25 15:05:59 -0300

    Até aula 37

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
-
 
 class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
     conteudo = models.TextField()
-    data_publicacao = models.DateTimeField(default=datetime.now())
-    
+    data_publicacao = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.titulo
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello, World!!!!</h1>")
+    posts= Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+#    except Post.DoesNotExist:
+#        return HttpResponse(f"Post com id {post_id} não existe.")
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_detail.html', context)
     
